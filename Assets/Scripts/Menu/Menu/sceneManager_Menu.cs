32a5bce32a87a3cf7ee5988480e6a394ed4705e9
shpx <gabriellva2007@gmail.com>
2024-08-07 15:08:43 -0300

    Menu parcialmente arrumado

--- Assets/Scripts/Menu/Menu/sceneManager_Menu.cs
+++ Assets/Scripts/Menu/Menu/sceneManager_Menu.cs
@@ -5,33 +5,66 @@ using UnityEngine.SceneManagement;
 
 public class sceneManager_Menu : MonoBehaviour
 {
+    public void Sair()
+    {
+        Application.Quit();
+    }
+    public void VoltarMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
     public void Jogar()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(1);
     }
 
     public void Tutorial()
     {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(2);
     }
 
     public void CampoDeTreinamento()
     {
-        SceneManager.LoadScene(4);
+        SceneManager.LoadScene(3);
     }
 
     public void Personagens()
     {
-        SceneManager.LoadScene(5);
+        SceneManager.LoadScene(4);
     }
 
     public void Ajustes()
     {
+        SceneManager.LoadScene(5);
+    }
+
+    public void Jogar_Configuracoes()
+    {
         SceneManager.LoadScene(6);
     }
 
-    public void Sair()
+    public void Jogar_Mapa()
     {
-        Application.Quit();
+        SceneManager.LoadScene(7);
+    }
+
+    public void Jogar_Personagens()
+    {
+        SceneManager.LoadScene(8);
+    }
+
+    public void Ajustes_Som()
+    {
+        SceneManager.LoadScene(9);
+    }
+
+    public void Ajustes_Controles()
+    {
+        SceneManager.LoadScene(10);
+    }
+
+    public void Ajustes_Video()
+    {
+        SceneManager.LoadScene(11);
     }
 }
