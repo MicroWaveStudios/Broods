1fe3df4750b17f2072328d5f8337be2c186850de
Caradofogo-04 <matheusoares2006@hotmail.com>
2024-09-29 16:38:25 -0300

    teste

--- Assets/Scripts/Gameplay/GameController.cs
+++ Assets/Scripts/Gameplay/GameController.cs
@@ -43,13 +43,10 @@ public class GameController : MonoBehaviour
     [SerializeField] GameObject PanelManager;
 
     PlayerInputManager playerInputManager;
-    PersonagensManager personagensManager;
 
     private void Start()
     {
-        personagensManager = GetComponent<PersonagensManager>();
         playerInputManager = GetComponent<PlayerInputManager>();
-
         //if (Pontos.pontosP1 != 0 || Pontos.pontosP2 != 0)
         //{
         //    playerInputManager.DisableJoining();
@@ -64,8 +61,10 @@ public class GameController : MonoBehaviour
 
         if (Player1 != null)
         {
-            playerInputManager.playerPrefab = PrefabPlayer[personagensManager.indexPersonagem1];
+            playerInputManager.playerPrefab = PrefabPlayer[0];
         }
+
+
 
         if (Player1 != null && Player2 != null)
         {
